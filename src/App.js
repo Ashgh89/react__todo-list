a85c9ca27b127ed16eba1aa862f431099671cced
Iman Ghanei <ghaneiash@gmail.com>
2022-03-07 15:16:57 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reaactjs.org"
+          href="https://reaacttjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
