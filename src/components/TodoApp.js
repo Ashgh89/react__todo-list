951c414bb93dd5e82fac84d099701131e07e76ff
Iman Ghanei <ghaneiash@gmail.com>
2022-03-08 15:46:14 +0100

    changes

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -19,6 +19,11 @@ const TodoApp = () => {
   const completeToDo = (id) => {
     const index = todos.findIndex((todo) => todo.id === id);
     console.log(index);
+    const selectedTodo = { ...todos[index] };
+    selectedTodo.isCompleted = !selectedTodo.isCompleted;
+    const updatedTodos = [...todos];
+    updatedTodos[index] = selectedTodo;
+    setTodos(updatedTodos);
   };
 
   return (
