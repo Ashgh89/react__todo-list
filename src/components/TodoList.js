94f84ad296db533bfe9e9ddad1419133281be132
Iman Ghanei <ghaneiash@gmail.com>
2022-03-10 15:13:04 +0100

    changes

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,24 +1,28 @@
+import { useState } from "react";
 import Todo from "./Todo";
+import TodoForm from "./TodoForm";
 
-const TodoList = ({ todos, onComplete, onDelete, onEdit }) => {
-  if (todos.length === 0) return <p>add some todos</p>;
+const TodoList = ({ todos, onComplete, onDelete }) => {
+  const [edit, setEdit] = useState({ id: null, text: "", isCompleted: false });
 
-  return (
-    <div>
-      {todos.map((todo) => {
-        return (
-          <Todo
-            key={todo.id}
-            todo={todo}
-            // onEdit={onEdit}
-            onComplete={() => onComplete(todo.id)}
-            onDelete={() => onDelete(todo.id)}
-            onEdit={() => onEdit(todo.id)}
-          />
-        );
-      })}
-    </div>
-  );
+  const renderTodos = () => {
+    if (todos.length === 0) return <p>add some todos</p>;
+
+    return todos.map((todo) => {
+      return (
+        <Todo
+          key={todo.id}
+          todo={todo}
+          // onEdit={onEdit}
+          onComplete={() => onComplete(todo.id)}
+          onDelete={() => onDelete(todo.id)}
+          onEdit={() => setEdit(todo)}
+        />
+      );
+    });
+  };
+
+  return <div>{edit.id ? <TodoForm /> : renderTodos()}</div>;
 };
 
 export default TodoList;
