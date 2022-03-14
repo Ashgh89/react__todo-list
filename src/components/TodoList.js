2afc85d6ebb032bfba9d8128f8d7dcbea767cbaa
Iman Ghanei <ghaneiash@gmail.com>
2022-03-14 12:49:08 +0100

    changes

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -11,7 +11,7 @@ const TodoList = ({ todos, onComplete, onDelete, onUpdate }) => {
   };
 
   const renderTodos = () => {
-    if (todos.length === 0) return <p>add someeeeeee todos</p>;
+    if (todos.length === 0) return <p>add somefüsfdsdf todos</p>;
 
     return todos.map((todo) => {
       return (
