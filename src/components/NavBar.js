ccbfe8ab0cb9cfe27e1ba94fa7efd3255a7ada8a
Iman Ghanei <ghaneiash@gmail.com>
2022-03-22 14:13:19 +0100

    changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,18 @@
+import { useState } from "react";
+
 const NavBar = ({ unCompletedTodos }) => {
+  const [status, setStatus] = useState("All");
+  const changeHandler = (e) => {
+    setStatus(e.target.value);
+  };
   if (!unCompletedTodos) return <h2>set your today todos!</h2>;
   return (
     <header>
       <span>{unCompletedTodos}</span> <h2>not completed</h2>
-      <select>
-        <option value="All"></option>
-        <option value="Completed"></option>
-        <option value="Uncompleted"></option>
+      <select onChange={changeHandler} value={status}>
+        <option value="All">All</option>
+        <option value="Completed">Completed</option>
+        <option value="Uncompleted">Uncompleted</option>
       </select>
     </header>
   );
