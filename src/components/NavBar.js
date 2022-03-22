608dddf33699898067f7b017049a81ceb4190bf9
Iman Ghanei <ghaneiash@gmail.com>
2022-03-22 14:05:10 +0100

    changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,18 @@
 const NavBar = ({ unCompletedTodos }) => {
+  if (!unCompletedTodos) return <h2>set your today todos!</h2>;
   return (
     <header>
-      {unCompletedTodos ? (
-        <>
-          <span>{unCompletedTodos}</span> <h2>not completed</h2>
-        </>
-      ) : (
-        <h2>set your today todos!</h2>
-      )}
+      <span>{unCompletedTodos}</span> <h2>not completed</h2>
+      <select>
+        <option value="All"></option>
+        <option value="Completed"></option>
+        <option value="Uncompleted"></option>
+      </select>
     </header>
   );
 };
 
 export default NavBar;
+// LET'S REFACTOR THIS
+
+// NOW LETS'S GIVE IT SELECT OPTION
